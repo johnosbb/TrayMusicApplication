67f8de3900e8aa0e0bbf22c388389329e3086fe5
johnosbb <johnos37@yahoo.com>
2020-12-27 11:54:39 +0000

    Adding files

---
+++ create_respository.sh
@@ -0,0 +1,7 @@
+echo "# TrayMusicApplication" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/johnosbb/TrayMusicApplication.git
+git push -u origin main
